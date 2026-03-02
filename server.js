29049f8eaa36811afd3c3c8079637687af79857b
Daniel Morales <102494301+DanielMorales24@users.noreply.github.com>
2026-03-02 10:02:23 -0600

    Enable serving static files from 'public' directory
    Added static file serving from the 'public' directory.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
@@ -8,6 +9,9 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Servir archivos
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Import variables
 const ingestRoutes = require('./routes/ingest.routes');
